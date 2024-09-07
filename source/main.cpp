fbcff463efb932ba702fbd683aaf9de2e95773ec
orientiered <da.morgachev@mail.ru>
2024-09-07 18:41:22 +0300

    Made sort function more universal

--- source/main.cpp
+++ source/main.cpp
@@ -5,19 +5,25 @@
 const int ROWS_NUMBER = 1024;
 const int LINE_LEN = 512;
 
-int readFromFile(const char* fileName, char * text);
-void bubbleSort(char *text, int (*cmp)(const char *first, const char *second));
-void printText(char *text);
+int readFromFile(const char* fileName, char *text);
+void bubbleSort(void *array, int elemSize, int length, int (*cmp)(const void *first, const void *second));
+void printText(int *index, char *text);
+// int strindexcmp(const int firstIdx, const int secondIdx);
+int strvoidcmp(const void *firstStr, const void *secondStr);
 
 int main() {
     char text[ROWS_NUMBER][LINE_LEN] = {};
+    int index[ROWS_NUMBER] = {};
+    for (size_t i = 0; i < ROWS_NUMBER; i++)
+        index[i] = i;
+
     if (readFromFile("Romeo and Juliet.txt", (char*) text)) {
         fprintf(stderr, "Can't read from file\n");
         return 0;
     }
-    bubbleSort((char*) text, stralphacmp);
+    bubbleSort((char*)text, LINE_LEN, ROWS_NUMBER, strvoidcmp);
 
-    printText((char*) text);
+    printText(index, (char *) text);
 }
 
 
@@ -43,13 +49,13 @@ int readFromFile(const char* fileName, char * text) {
 }
 
 
-void bubbleSort(char *text, int (*cmp)(const char *first, const char *second)) {
-    for (int rightBoundary = ROWS_NUMBER; rightBoundary > 0; rightBoundary--) {
+void bubbleSort(void *array, int elemSize, int length, int (*cmp)(const void *first, const void *second)) {
+    for (int rightBoundary = length; rightBoundary > 0; rightBoundary--) {
         unsigned char swapFlag = 0; //if we didn't swap anything, array is sorted
         for (int i = 0; i < rightBoundary-1; i++) {
-            if (cmp(text + i*LINE_LEN, text + (i+1)*LINE_LEN) > 0) {
+            if (cmp((char*)array + i*elemSize, (char*)array + (i+1)*elemSize) > 0) {
                 swapFlag = 1;
-                swap(text + i*LINE_LEN, text + (i+1)*LINE_LEN, LINE_LEN);
+                swap((char*)array + i*elemSize, (char*)array + (i+1)*elemSize, elemSize);
             }
         }
         if (!swapFlag) break;
@@ -57,7 +63,11 @@ void bubbleSort(char *text, int (*cmp)(const char *first, const char *second)) {
 }
 
 
-void printText(char *text) {
+int strvoidcmp(const void *firstStr, const void *secondStr) {
+    return stralphacmp((const char*) firstStr, (const char*) secondStr);
+}
+
+void printText(int *index, char *text) {
     for (size_t rowIndex = 0; rowIndex < ROWS_NUMBER; rowIndex++)
-        printf("%s", text + rowIndex*LINE_LEN);
+        printf("%s", text + index[rowIndex]*LINE_LEN);
 }
